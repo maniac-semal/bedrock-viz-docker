bbdf7721046b8e2bc7d68b1d22098f5ee4e332f3
MaNIaC-Semal <maniac@themania.org>
2022-01-30 16:54:33 +0100

    corrected startup settings

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+mkdir -p /appdata
+mkdir -p /appdata/out
+mkdir -p /appdata/world
+mkdir -p /input
+
 cp -R /appdata/out/* /usr/share/nginx/html
 
 /etc/init.d/nginx start
+/etc/init.d/cron start
 
 /opt/scripts/cron.sh
 
