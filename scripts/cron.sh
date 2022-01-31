9a2f75216985c5e6b96800ebe730fd6c8a408ee6
MaNIaC-Semal <maniac@themania.org>
2022-01-31 08:06:44 +0100

    Added comments, corrected Logfile emptying

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
 
 
+echo "----------------------------------------------"
+echo "Copying current world to working folder"
+# Copy current world to working folder
 cp -R /input/* /appdata/world
+echo "----------------------------------------------"
 
+# Check logile size
+
+#    File to consider
+LOGFILE=/appdata/out/bedrock_viz.log
+
+#    MAXSIZE is 50 MB
+MAXSIZE=5000000
+
+#    Get file size
+FILESIZE=$(stat -c%s "$LOGFILE")
+
+# Check filesize of Logfile and empty if too big.
+if (( $FILESIZE > $MAXSIZE)); then
+    echo "" > $LOGFILE
+    echo "----------------------------------------------"
+    echo "Max Logfile filesize reached, emptied"
+    echo "----------------------------------------------"
+fi
+
+echo "----------------------------------------------"
+echo "Starting new generation of Map"
 /usr/local/bin/bedrock-viz --cfg /appdata/data/bedrock_viz.cfg --xml /appdata/data/bedrock_viz.xml --db /appdata/world --out /appdata/out --html-all --quiet
 
 cp -R /appdata/out/* /usr/share/nginx/html
+echo "New map generated and updated in WebUi"
+echo "----------------------------------------------"
--- scripts/start.sh
+++ scripts/start.sh
@@ -6,19 +6,28 @@ mkdir -p /appdata/world
 mkdir -p /appdata/data
 mkdir -p /input
 
+echo "----------------------------------------------"
+echo "Copying config files to appdata if not present"
 cp -n /usr/local/share/bedrock-viz/data/bedrock_viz.cfg /appdata/data/bedrock_viz.cfg
 cp -n /usr/local/share/bedrock-viz/data/bedrock_viz.xml /appdata/data/bedrock_viz.xml
+echo "----------------------------------------------"
 
+echo "----------------------------------------------"
+echo "Setting up nginx and cron services"
 # Implement settings for cron and nginx
 crontab -u $(whoami) -r
 line="*/"$TIMEFRAME" * * * * /opt/scripts/cron.sh"
 (crontab -u $(whoami) -l; echo "$line" ) | crontab -u $(whoami) -
 cp /opt/scripts/bedrock_viz.conf /etc/nginx/conf.d/
+echo "----------------------------------------------"
 
 cp -R /appdata/out/* /usr/share/nginx/html
 
+echo "----------------------------------------------"
+echo "Starting nginx and cron services"
 /etc/init.d/nginx start
 /etc/init.d/cron start
+echo "----------------------------------------------"
 
 /opt/scripts/cron.sh
 
