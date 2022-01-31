bc85862da10d41218bd44f51d4b9a11e1b69940a
MaNIaC-Semal <maniac@themania.org>
2022-01-31 23:06:58 +0100

    corrected spacing in logs

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -8,11 +8,11 @@ function exec_dockerlog() {
         "$@"  | sed -e "s/^/[$(date '+%Y-%m-%d %H:%M:%S')]/" > /proc/1/fd/1
 }
 
-echo_dockerlog "\n----------------------------------------------"
+echo_dockerlog "----------------------------------------------"
 echo_dockerlog "Copying current world to working folder"
 # Copy current world to working folder
 cp -R /input/* /appdata/world
-echo_dockerlog "----------------------------------------------"
+echo_dockerlog "----------------------------------------------\n"
 
 # Check logile size
 
@@ -28,16 +28,16 @@ FILESIZE=$(stat -c%s "$LOGFILE")
 # Check filesize of Logfile and empty if too big.
 if (( $FILESIZE > $MAXSIZE)); then
     echo "" > $LOGFILE
-    echo_dockerlog "\n----------------------------------------------"
-    echo_dockerlog "Max Logfile filesize reached, emptied"
     echo_dockerlog "----------------------------------------------"
+    echo_dockerlog "Max Logfile filesize reached, emptied"
+    echo_dockerlog "----------------------------------------------\n"
 fi
 
-echo_dockerlog "\n----------------------------------------------"
+echo_dockerlog "----------------------------------------------"
 echo_dockerlog "Starting new generation of Map"
 
 exec_dockerlog /usr/local/bin/bedrock-viz --cfg /appdata/data/bedrock_viz.cfg --xml /appdata/data/bedrock_viz.xml --db /appdata/world --out /appdata/out --html-all --quiet
 cp -R /appdata/out/* /usr/share/nginx/html
 
 echo_dockerlog "New map generated and updated in WebUI"
-echo_dockerlog "----------------------------------------------"
+echo_dockerlog "----------------------------------------------\n"
--- scripts/start.sh
+++ scripts/start.sh
@@ -14,28 +14,28 @@ mkdir -p /appdata/world
 mkdir -p /appdata/data
 mkdir -p /input
 
-echo_dockerlog "\n----------------------------------------------" 
+echo_dockerlog "----------------------------------------------" 
 echo_dockerlog "Copying config files to appdata if not present"
 cp -n /usr/local/share/bedrock-viz/data/bedrock_viz.cfg /appdata/data/bedrock_viz.cfg
 cp -n /usr/local/share/bedrock-viz/data/bedrock_viz.xml /appdata/data/bedrock_viz.xml
-echo_dockerlog "----------------------------------------------"
+echo_dockerlog "----------------------------------------------\n"
 
-echo_dockerlog "\n----------------------------------------------"
+echo_dockerlog "----------------------------------------------"
 echo_dockerlog "Setting up nginx and cron services"
 # Implement settings for cron and nginx
 crontab -u $(whoami) -r
 line="*/"$TIMEFRAME" * * * * /opt/scripts/cron.sh"
 (crontab -u $(whoami) -l; echo "$line" ) | crontab -u $(whoami) -
 cp /opt/scripts/bedrock_viz.conf /etc/nginx/conf.d/
-echo_dockerlog "----------------------------------------------"
+echo_dockerlog "----------------------------------------------\n"
 
 cp -R /appdata/out/* /usr/share/nginx/html
 
-echo_dockerlog "\n----------------------------------------------"
+echo_dockerlog "----------------------------------------------"
 echo_dockerlog "Starting nginx and cron services"
 exec_dockerlog etc/init.d/nginx start
 exec_dockerlog /etc/init.d/cron start
-echo_dockerlog "----------------------------------------------"
+echo_dockerlog "----------------------------------------------\n"
 
 /opt/scripts/cron.sh
 
